28f01c62f2e06b84ae2994d6f7e22ba50f22a12e
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-11-10 22:58:00 -0800

    first commit

--- javakws.s
+++ javakws.s
@@ -1,13 +1,16 @@
-#  Name:   Nguyen, Peter
-#  Homework:  # 4
-#  Due:        11/10/2022
-#  Course:  cs-2640-04-f22 
+# 
+#  Name:	Nguyen, Peter
+#  Homework:	4
+#  Due:		11/10/2022 
+#  Course:	cs-2640-04-f22 
 # 
 #  Description: 
-#    Goes through java keywords and finds which number it is
-
-
+#    Takes argument through command line and searches through array to find out which index it is
+# 
+	
 	.data
+intro:		.asciiz "Java Keywords by P. Nguyen\n\n"
+
 keywords:
 	.word	abstract, assert, boolean, xbreak, byte, case, catch, char
 	.word	class, const, continue, default, do, double, else, enum
@@ -17,143 +20,171 @@ keywords:
 	.word	strictfp, super, switch, synchronized, this, throw, throws, transient
 	.word	true, try, void, volatile, while
 
-intro:    .asciiz   "Java Keywords by P. Nguyen\n\n"
 abstract:	.asciiz	"abstract"
-assert:	.asciiz	"assert"
+assert:		.asciiz	"assert"
 boolean:	.asciiz	"boolean"
-xbreak:	.asciiz	"break"
-byte:	.asciiz	"byte"
-case:	.asciiz	"case"
-catch:	.asciiz	"catch"
-char:	.asciiz	"char"
-class:	.asciiz	"class"
-const:	.asciiz	"const"
+xbreak:		.asciiz	"break"
+byte:		.asciiz	"byte"
+case:		.asciiz	"case"
+catch:		.asciiz	"catch"
+char:		.asciiz	"char"
+class:		.asciiz	"class"
+const:		.asciiz	"const"
 continue:	.asciiz	"continue"
 default:	.asciiz	"default"
-do:	.asciiz	"do"
-double:	.asciiz	"double"
-else:	.asciiz	"else"
-enum:	.asciiz	"enum"
+do:		.asciiz	"do"
+double:		.asciiz	"double"
+else:		.asciiz	"else"
+enum:		.asciiz	"enum"
 extends:	.asciiz	"extends"
-false:	.asciiz	"false"
-final:	.asciiz	"final"
+false:		.asciiz	"false"
+final:		.asciiz	"final"
 finally:	.asciiz	"finally"
-float:	.asciiz	"float"
-for:	.asciiz	"for"
-goto:	.asciiz	"goto"
-if:	.asciiz	"if"
-implements:
-	.asciiz	"implements"
-import:	.asciiz	"import"
-instanceof:
-	.asciiz	"instanceof"
-int:	.asciiz	"int"
-interface:
-	.asciiz	"interface"
-long:	.asciiz	"long"
-native:	.asciiz	"native"
-new:	.asciiz	"new"
-null:	.asciiz	"null"
+float:		.asciiz	"float"
+for:		.asciiz	"for"
+goto:		.asciiz	"goto"
+if:		.asciiz	"if"
+implements:	.asciiz	"implements"
+import:		.asciiz	"import"
+instanceof:	.asciiz	"instanceof"
+int:		.asciiz	"int"
+interface:	.asciiz	"interface"
+long:		.asciiz	"long"
+native:		.asciiz	"native"
+new:		.asciiz	"new"
+null:		.asciiz	"null"
 package:	.asciiz	"package"
 private:	.asciiz	"private"
-protected:
-	.asciiz	"protected"
-public:	.asciiz	"public"
-return:	.asciiz	"return"
-short:	.asciiz	"short"
-static:	.asciiz	"static"
+protected:	.asciiz	"protected"
+public:		.asciiz	"public"
+return:		.asciiz	"return"
+short:		.asciiz	"short"
+static:		.asciiz	"static"
 strictfp:	.asciiz	"strictfp"
-super:	.asciiz	"super"
-switch:	.asciiz	"switch"
-synchronized:
-	.asciiz	"synchronized"
-this:	.asciiz	"this"
-throw:	.asciiz	"throw"
-throws:	.asciiz	"throws"
-transient:
-	.asciiz	"transient"
-true:	.asciiz	"true"
-try:	.asciiz	"try"
-void:	.asciiz	"void"
+super:		.asciiz	"super"
+switch:		.asciiz	"switch"
+synchronized:	.asciiz	"synchronized"
+this:		.asciiz	"this"
+throw:		.asciiz	"throw"
+throws:		.asciiz	"throws"
+transient:	.asciiz	"transient"
+true:		.asciiz	"true"
+try:		.asciiz	"try"
+void:		.asciiz	"void"
 volatile:	.asciiz	"volatile"
-while:	.asciiz	"while"
+while:		.asciiz	"while"
+
 
 
 	.text
 main:
-	li	$v0, 4         #prints out the intro
-	la	$a0, intro
-     syscall
-	move	$t0, $a0
-	move	$t1, $a1
+	move	$s0, $a0	# s0 = argc
+	move	$s1, $a1	# s1 = argv, address to an array of cstring
+	sub	$s0, $s0, 1	# removing 1 so it doesn't count path to args
+	addiu	$s1, $s1, 4	# adding 4 so it skips path and goes to first argument
 
-doagain:	
-	lw	$a0, ($t1)	# loop and output all argv
+	la	$a0, intro
 	li	$v0, 4
 	syscall
 	li	$a0, '\n'
 	li	$v0, 11
 	syscall
 
-	addiu	$t1, $t1, 4	# next argv
-	sub	$t0, $t0, 1
-	bnez	$t0, doagain
 
-	li	$v0, 10
+
+dowhile:
+	la	$a0, keywords
+	li	$a1, 53
+	lw	$a2, ($s1)	# loop and output all argv
+
+	jal	lsearch
+
+	bge	$v0, 53, else2	# if the return value is greater than the length => else2
+
+	move	$t4, $v0	# prints
+	la	$t2, keywords
+	sll	$t5, $v0, 2
+	addu	$t3, $t2, $t5	
+	lw	$a0, ($t3)
+	li	$v0, 4
+	syscall
+	li	$a0, ':'
+	li	$v0, 11
+	syscall
+	move	$a0, $t4
+	li 	$v0, 1
+	syscall
+	li	$a0, '\n'
+	li	$v0, 11
+	syscall
+	b endif3
+else2:				# doesn't exist in array
+	lw	$a0, ($s1)	# prints word
+	li	$v0, 4
+	syscall
+	li	$a0, '\n'
+	li	$v0, 11
+	syscall
+endif3:
+	addiu	$s1, $s1, 4	# next argv
+	sub	$s0, $s0, 1
+	bnez	$s0, dowhile
+
+
+	
+	li	$a0, '\n'
+	li	$v0, 11
+	syscall
+	li	$v0, 10		# exit
 	syscall
 
+lsearch:
+	addiu	$sp, $sp, -4
+	sw	$ra, ($sp)
+	move	$s2, $a0		# s2 = keyword array address
+	move	$s3, $a1		# s3 = length
+
+	li	$s4, 0			# index
+
+while1:	bge	$s4, $s3, endwhile1	# index >= 52
+
+
+	lw	$a0, ($s2)		# address of string
+	move	$a1, $a2		# address to value
+	jal	strcmp
+
+	bnez	$v0, else1	
+	b	endwhile1
+else1:
+	addi	$s4, $s4, 1
+	addi	$s2, $s2, 4
+
+	b	while1
+
+endwhile1:
+
+	lw	$ra, ($sp)
+	addiu	$sp, $sp, 4
+	move	$v0, $s4
+	jr	$ra
 
 strcmp:
-            # a0 = strkeyword , a1 = strinput
-forLoop:
-	lb	$t1, ($a0)    # t1 = strkeyword[i] , a char of strkeyword
-	lb	$t2, ($a1)    # t2 = strinput[i], a char of strinput
-	bne	$t1, $t2, endfor    # while(strkeyword[i]==strinput[i])
+	li	$t0, 0			# int i = 0
 
-	beqz	$t1, endif    # if(strkeyword[i]==\0) go to endif
+while2:
+	addu	$t1, $a0, $t0
+	addu	$t2, $a1, $t0
 
-	add	$a0, 1    # i++
-	add	$a1, 1
-	b	forLoop    
-endfor:
-	sub	$v0, $t1, $t2    # return strkeyword[i] - strinput[i]
-	jr	$ra
-endif:
-	li	$v0, 0    #return 0
-	jr	$ra 
+	lb	$t3, ($t1)		# loads characters into t3 and t4
+	lb	$t4, ($t2)
+	bne	$t3, $t4, endwhile2	# t3 != t4 => endwhile2
+	bne	$t3, $zero, endif2	# t3 != '\0' => endif2
+	li	$v0, 0
+	b	endwhile2
+endif2:
+	addi	$t0, $t0, 1		# i++
+	b while2
 
-lsearch:
-					# a0 = keywords[], a1 = length, a2 = inputstring[]
-	addiu $sp,$sp,-4    # push(ra) b/c will call a procedure later
-	sw	$ra, 0($sp)
-
-	move	$s1, $a0    # s1 = keywords[], b/c calling procedure later so args will change
-	move	$s2, $a1    # s2 = length
-	move	$s3, $a2    # s3 = inputstring[]	
-	li	$s0, 0    # index = 0
-	li	$s4, 0    # s4 = false, if found or not
-whileLoop2:
-	beq	$s4, 1, endWhile2    # while(!found && index<length)
-	bge	$s0, $t3,endWhile2    
-
-	sll	$s5, $t0, 2    #offset = index*4
-	add	$s5, $s1, $s5    # effect addr = baseadr + offset 
-	lw	$s5, ($s5)    # s5 = keywords[i] 
-
-	move	$a0,$s5    # strcmp(keywords[i],length)
-	move	$a1,$s3
-	jal	strcmp    # v0 = 0 if strings same
-
-	bnez	$v0, else1    # if( strcmp(keywords[i],length) == 0)
-	li	$s4, 1    # s0 = true, keyword is found
-	b	endif
-else1:
-	add	$s0, 1    # index++
-endif2:    
-	b	whileLoop2
-endWhile2:
-
-	lw	$ra,0($sp)    # pop(), ra = back to main
-	addiu $sp,$sp,4
-
+endwhile2:
+	sub	$v0, $t3, $t4		# return s[i] - t[i]
 	jr	$ra
